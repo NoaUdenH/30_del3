7d5c2f26e10a4be3b022aac70c6bb8a36c25d9ff
s185736 <s185736@student.dtu.dk>
2021-11-18 13:36:36 +0100

    Opdatering: Feltnavnene er opdateret.

--- src/dasuki/Felt/SpilInfo.java
+++ src/dasuki/Felt/SpilInfo.java
@@ -7,28 +7,28 @@ public class SpilInfo {
 
     public static void openGameBoard() {
         addHus(1, "Start", "Beskrivelse..", 0, Color.WHITE);
-        addHus(2, "Felt1", "Beskrivelse..", 1, Color.WHITE);
-        addHus(3, "Felt2", "Beskrivelse..", 1, Color.WHITE);
-        addHus(4, "Felt3", "Beskrivelse..", 0, Color.WHITE);
-        addHus(5, "Felt4", "Beskrivelse..", 1, Color.WHITE);
-        addHus(6, "Felt5", "Beskrivelse..",1, Color.WHITE);
-        addHus(7, "Felt6", "Beskrivelse..",0,Color.WHITE);
-        addHus(8, "Felt7", "Beskrivelse..",2,Color.WHITE);
-        addHus(9, "Felt8", "Beskrivelse..", 2, Color.WHITE);
-        addHus(10, "Felt9", "Beskrivelse..",0, Color.WHITE);
-        addHus(11, "Felt10", "Beskrivelse..", 2, Color.WHITE);
-        addHus(12, "Felt11", "Beskrivelse..", 2, Color.WHITE);
-        addHus(13, "Felt12", "Beskrivelse..",0, Color.WHITE);
-        addHus(14, "Felt13", "Beskrivelse..", 3, Color.WHITE);
-        addHus(15, "Felt14", "Beskrivelse..", 3, Color.WHITE);
-        addHus(16, "Felt15", "Beskrivelse..",0, Color.WHITE);
-        addHus(17, "Felt16", "Beskrivelse..", 3, Color.WHITE);
-        addHus(18, "Felt17", "Beskrivelse..", 3, Color.WHITE);
-        addHus(19, "Felt18", "Beskrivelse..",0, Color.WHITE);
-        addHus(20, "Felt19", "Beskrivelse..", 4, Color.WHITE);
-        addHus(21, "Felt20", "Beskrivelse..", 4, Color.WHITE);
-        addHus(22, "Felt21", "Beskrivelse..",0, Color.WHITE);
-        addHus(23, "Felt22", "Beskrivelse..", 5, Color.WHITE);
-        addHus(24, "Felt23", "Beskrivelse..", 5, Color.WHITE);
+        addHus(2, "BURGERBAREN", "Du ser sulten ud, få købt dig en burger..!", 1, new Color(189, 122, 55));
+        addHus(3, "PIZZARIAET", "Du ligner en der har brug for en ordentlig Shawarma..!", 1, new Color(189, 122, 55));
+        addHus(4, "CHANCE", "Du får en chance..", 0, Color.WHITE);
+        addHus(5, "SLIKBUTIKKEN", "Få dig noget sødt her i slikbutikken..", 1, new Color(127, 196, 217));
+        addHus(6, "ISKIOSKEN", "Få dig noget der er cremet samtidig sødt..",1, new Color(127, 196, 217));
+        addHus(7, "I FÆNGSEL\nPAA BESØG", "Ved du hvor du er henne..?",0, Color.GRAY);
+        addHus(8, "MUSEET", "Se hvor smukt det er her i museet..",2, new Color(203, 104, 226));
+        addHus(9, "BIBLIOTEKET", "Få noget viden..", 2, new Color(203, 104, 226));
+        addHus(10, "CHANCE", "Du får endnu en chance..",0, Color.GRAY);
+        addHus(11, "SKATEPARKEN", "Få dig hygget lidt med kammeraterne på skateparken..", 2, new Color(220, 129, 38));
+        addHus(12, "SWIMMINGPOOLEN", "Keder du dig, så dyk ned i poolen..", 2, new Color(220, 129, 38));
+        addHus(13, "GRATIS PARKERING\n", "Du kan parkere her uden bekymringer..",0, Color.WHITE);
+        addHus(14, "SPILLEHALLEN", "Er det en god ide..?", 3, new Color(140, 5, 5));
+        addHus(15, "BIOGRAFEN", "Det ville være rart, at få set en film..", 3, new Color(140, 5, 5));
+        addHus(16, "CHANCE", "Du får squ endnu en chance..",0, Color.GRAY);
+        addHus(17, "LEGETØJSBUTIKKEN", "Gør' dine yngre glad..", 3, new Color(255, 213, 0));
+        addHus(18, "DYREHANDLEN", "Brug nogle penge!", 3, new Color(255, 213, 0));
+        addHus(19, "GÅ I FÆNGSEL", "Oops, du er havnet i fængslet!",0, Color.GRAY);
+        addHus(20, "BOWLINGHALLEN", "Få hygget lidt..", 4, new Color(10, 157, 22));
+        addHus(21, "ZOO", "Ville være rat at komme ned med familien..", 4, new Color(10, 157, 22));
+        addHus(22, "CHANCE", "Du får endnu en chance..",0, Color.GRAY);
+        addHus(23, "VANDLANDET", "Du er havnet her i vandlandet!", 5, new Color(49, 90, 164));
+        addHus(24, "STRANDPROMENADEN", "Strandpromenaden, hvad laver du her..?", 5, new Color(49, 90, 164));
     }
 }
